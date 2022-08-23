8efcd7eaed3f2dad3238d79c9a298941932647f3
fattycat <dreamworld2010@126.com>
2022-08-23 11:07:08 +0800

    fix & update

--- lua/init.lua
+++ lua/init.lua
@@ -26,9 +26,19 @@ require("packer").startup(function(use)
 
     use {
         "kyazdani42/nvim-tree.lua",
-        requires = {"kyazdani42/nvim-web-devicons"}
+        requires = {"kyazdani42/nvim-web-devicons"},
         config = function()
-            require("nvim_tree")
+            require("nvim-tree").setup({
+                update_cwd = true,
+                view = {
+                    mappings = {
+                        list = {
+                            {key = "<C-e>", action = ""},
+                            {key = "s", action = "vsplit"}
+                        }
+                    }
+                }
+            })
         end
     }
     use {'neoclide/coc.nvim', branch = 'master'}
@@ -36,7 +46,7 @@ require("packer").startup(function(use)
     use {
         "nvim-telescope/telescope.nvim",
         tag = "0.1.0",
-        requires = {"nvim-lua/plenary.nvim"}
+        requires = {"nvim-lua/plenary.nvim"},
         config = function()
             require('telescope').load_extension('projects')
         end
